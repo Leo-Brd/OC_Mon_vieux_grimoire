1e6c83c9cb912dd23d9a71e9c550d64935ee3dee
Leo-Brd <leobordet.pro@gmail.com>
2025-01-08 16:16:29 +0100

    Add first get middleware

--- backend/app.js
+++ backend/app.js
@@ -26,4 +26,11 @@ app.post('/api/books', (req, res, next) => {
   next();
 });
 
+app.get('/api/books', (req, res, next) => {
+  Book.find()
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json({ error }));
+  next();
+});
+
 module.exports = app;
