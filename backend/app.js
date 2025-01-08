2bb01f478cb4cf7336613cede6abcc22dc415e83
Leo-Brd <leobordet.pro@gmail.com>
2025-01-08 15:33:07 +0100

    Add app headers

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,15 @@ const mongoose = require('mongoose');
 const app = express();
 
 mongoose.connect('mongodb+srv://LeoBrd:43LHxFbLd4X2f7oy@mon-vieux-grimoire.oltcf.mongodb.net/?retryWrites=true&w=majority&appName=Mon-vieux-grimoire')
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 app.use((req, res) => {
     res.json({ message: 'Votre requête a bien été recue !'})
