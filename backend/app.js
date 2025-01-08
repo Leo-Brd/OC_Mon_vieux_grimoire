e6ec99d594d26f9416866401d7a77cc2fb4a741f
Leo-Brd <leobordet.pro@gmail.com>
2025-01-08 13:33:36 +0100

    Add mongodb database to the app

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://LeoBrd:43LHxFbLd4X2f7oy@mon-vieux-grimoire.oltcf.mongodb.net/?retryWrites=true&w=majority&appName=Mon-vieux-grimoire')
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res) => {
     res.json({ message: 'Votre requête a bien été recue !'})
