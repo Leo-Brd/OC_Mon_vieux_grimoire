4802f6669eb4e8b921e8511221842e48aea5dc8a
Leo-Brd <leobordet.pro@gmail.com>
2025-01-10 15:11:53 +0100

    Change unique validation to a mongoose plugin

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
@@ -19,10 +20,6 @@ const userSchema = new Schema({
   timestamps: true,
 });
 
-// To verify that the email is unique
-userSchema.path('email').validate(async function (value) {
-  const count = await mongoose.models.User.countDocuments({ email: value });
-  return count === 0;
-}, 'Email already exists');
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User', userSchema);
