756c1b0047b496d660dfba361a7d8fc06dfea572
Leo-Brd <leobordet.pro@gmail.com>
2025-01-17 15:02:18 +0100

    Change createBook function to manage the image url

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 const bookRoutes = require('./routes/bookRoutes');
 const userRoutes = require('./routes/userRoutes');
 
@@ -21,5 +22,6 @@ app.use(bodyParser.json());
 
 app.use('/api/books', bookRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,14 +1,27 @@
 const Book = require('../models/bookModel');
 
 exports.createBook = (req, res, next) => {
-    delete req.body._id;
-    const book = new Book({
-        ...req.body
-    });
-    book.save()
-        .then(() => res.status(201).json({ message: 'Livre enregistré !'}))
-        .catch(error => res.status(400).json({ error }));
+  try {
+      const bookObject = JSON.parse(req.body.book);
+
+      delete bookObject._id;
+      delete bookObject._userId;
+
+      const book = new Book({
+          ...bookObject,
+          userId: req.auth.userId,
+          imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+          ratings: []
+      });
+
+      book.save()
+          .then(() => res.status(201).json({ message: 'Livre enregistré !' }))
+          .catch(error => res.status(400).json({ error }));
+  } catch (error) {
+      res.status(400).json({ error: 'Requête invalide' });
+  }
 };
+
 
 exports.getAllBooks = (req, res, next) => {
     Book.find()
